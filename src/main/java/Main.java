c1c30aee595504f0e20c6da5c1413f981a479ea2
Adrian Khaskin <haskin.dev@gmail.com>
2022-06-28 16:30:32 -0400

    Added lookupLibraryBook

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 import model.Book;
 import model.Library;
 
-// Ask the user if they want to add a book or look up an existing book
-// If they want to look up an existing book, ask them for the title of the book
-// Using the title they enter, look up the book in your map-based list
-// If you find the book, display the book's information and continue your loop
-// If you don't find the book, tell the user your library doesn't have that book and continue your loop
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(Library.getLibrary());
@@ -21,9 +15,9 @@ public class Main {
                 if (userChoice.equalsIgnoreCase("add"))
                     Library.addLibraryBooks(scanner);
                 else
-                    Library.look
+                    Library.lookupLibraryBook(scanner);
 
-                    System.out.print("Are you done with the library system? (Y/n): ");
+                System.out.print("\nAre you done with the library system? (Y/n): ");
                 boolean isDone = !scanner.nextLine().equals("n");
                 if (isDone)
                     return;
--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -10,6 +10,37 @@ public class Library {
     private Library() {
     }
 
+    /**
+     * Looks up books based by title.
+     * 
+     * @param scanner
+     */
+    public static void lookupLibraryBook(Scanner scanner) {
+        while (true) {
+            try {
+                System.out.println("\n---- Lookup Book ----\n");
+                System.out.print("Enter the book title: ");
+                String title = scanner.nextLine();
+                if (!Library.books.containsKey(title))
+                    System.out.println(String.format("Sorry, we couldn't find the book \"%s\"", title));
+                else
+                    System.out.println(Library.books.get(title));
+
+                System.out.print("Are you done looking up books?(Y/n): ");
+                Boolean isDone = !scanner.nextLine().equals("n");
+                if (isDone)
+                    return;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Adds book(s) from user input to book container
+     * 
+     * @param scanner
+     */
     public static void addLibraryBooks(Scanner scanner) {
         while (true) {
             String title = "", genre = "";
@@ -31,7 +62,7 @@ public class Library {
                 Book book = new Book(title, genre, numberOfPages);
                 Library.addBook(title, book);
 
-                System.out.print("Are you done adding books(Y/n): ");
+                System.out.print("Are you done adding books?(Y/n): ");
                 Boolean isDone = !scanner.nextLine().equals("n");
                 if (isDone)
                     return;
@@ -44,10 +75,6 @@ public class Library {
                     scanner.nextLine();
             }
         }
-    }
-
-    public static void lookupLibraryBook(Book book) {
-
     }
 
     public static void addBook(String title, Book book) {
